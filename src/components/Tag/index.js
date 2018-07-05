5db906bea671689c09a07ac78676c21b82fdff11
mrmeisen <ms915818993@163.com>
2018-07-05 15:33:48 +0800

    update

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as FontAwesome from 'react-icons/lib/fa';
 
 import './tag.scss';
 
@@ -15,7 +16,7 @@ class Tag extends React.PureComponent {
     render() {
         const {label} = this.props;
         return (
-            <span className='tag' onClick={() => this.clickTag()}>{label}</span>
+            <span className='tag' onClick={() => this.clickTag()}><FontAwesome.FaTag/> {label}</span>
         );
     }
 }
