f8119c20a737bf1adf17c6748155b92b3b7bb2af
mrmeisen <ms915818993@163.com>
2018-07-19 19:20:13 +0800

    update weibo refer

--- packages/blog/src/components/SideBar/index.js
+++ packages/blog/src/components/SideBar/index.js
@@ -17,7 +17,7 @@ class SideBar extends React.PureComponent {
                     <p className='bar-contact'>
                         <a className='bar-icon' href='https://github.com/mrmeisen' target='_blank'>
                             <FontAwesome.FaGithub/> <span>GitHub</span></a>
-                        <a className='bar-icon' href='https://weibo.com/3002849234/profile?rightmod=1&wvr=6&mod=personinfo'
+                        <a className='bar-icon' href='https://weibo.com/u/3002849234?refer_flag=1001030101_'
                            target='_blank'><FontAwesome.FaWeibo/> <span>WeiBo</span></a>
                     </p>
                 </section>
