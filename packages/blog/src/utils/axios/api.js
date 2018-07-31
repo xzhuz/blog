f06df0f46b60310ee9fae21e2c100bd4cb271baf
mrmeisen <ms915818993@163.com>
2018-07-31 13:12:49 +0800

    删除多余接口

--- packages/blog/src/utils/axios/api.js
+++ packages/blog/src/utils/axios/api.js
@@ -1,18 +1,12 @@
-import {get, post} from './http';
+import { get } from './http';
 
 
 // Article
-export const articleList = () => {
-    return get('/articles/list');
-};
 
 export const articleDetail = (id) => {
     return get('/articles/info', {id: id});
 };
 
-export const popularArticles = () => {
-    return get('/articles/popular');
-};
 
 export const countArticle = () => {
     return get('/articles/count');
@@ -29,29 +23,16 @@ export const achieveArticle = () => {
     return get('/articles/achieve');
 };
 
-export const publishArticle = ({thumb, content, summary, title, tags, visit, publish}) => {
-    return post('/articles/publish', {thumb, content, summary, title, tags: tags.join(','), visit, publish});
-};
 
 export const increaseVisit = (id) => {
     return get('/articles/visit', {id: id});
 };
 
-export const deleteArticle = (id) => {
-    return get('/articles/delete', {id: id});
-};
-
-export const updateArticle = ({id, content, summary, title, tags, publish, thumb}) => {
-    return post('/articles/update', {id, content, summary, title, tags: tags.join(','), publish, thumb});
-};
 
 export const relativeArticles = ({tag, page, size}) => {
     return get('/articles/relative', {tag: new Array(tag).join(','), page: page, size: size});
 };
 
-export const allArticleTags = () => {
-    return get('/articles/tags');
-};
 
 export const doConfirmCompliment = (id) => {
     return get('/articles/compliment/confirm', {id: id});
